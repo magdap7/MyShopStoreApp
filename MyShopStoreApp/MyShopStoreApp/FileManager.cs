4588e8872b0f076879402c48877113c01c961a4f
magda <magda@LAPTOP-JBFKPILU>
2023-02-21 14:33:22 +0100

    Zadanie 19 - pierwszy commit

---
+++ MyShopStoreApp/MyShopStoreApp/FileManager.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopStoreApp
+{
+    public class FileManager
+    {
+        const string header = "Nazwa;Cena jedn.;Ile sztuk;Masa lub obj.;Jednostka [kg, l]";
+        string fileName;
+        public FileManager(string name)
+        {
+            this.fileName = name;
+        }
+
+        public void AddLineToFile(List<string> list)
+        {
+            if (!File.Exists(fileName))
+            {//dodaj wiersz nazw kolumn
+                using (var writer = File.AppendText(fileName))
+                {
+                    writer.WriteLine(header);
+                }
+            }
+            using (var writer = File.AppendText(fileName))
+            {
+                string combinedString = string.Join(";", list.ToArray());
+                writer.WriteLine(combinedString);
+            }
+        }
+        public void AddLineToFile(string combinedString)
+        {
+            if (!File.Exists(fileName))
+            {//dodaj wiersz nazw kolumn
+                using (var writer = File.AppendText(fileName))
+                {
+                    writer.WriteLine(header);
+                }
+            }
+            using (var writer = File.AppendText(fileName))
+            {
+                writer.WriteLine(combinedString);
+            }
+        }
+        public List<string> ReadLineListFromFile()
+        {
+            List<string> list = new List<string>();
+            if (!File.Exists(fileName))
+            {//rzuć wyjątkiem
+                throw new Exception($"File {fileName} doesn't exist.");
+            }
+            else
+            {
+                using (var reader = File.OpenText(fileName))
+                {
+                    var line = reader.ReadLine();
+                    while (line != null)
+                    {
+                        list.Add(line);
+                        line = reader.ReadLine();
+                    }
+                }
+            }
+            return list;
+        }
+        void ClearFile()
+        {
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+        }
+
+
+
+    }
+}
---
+++ MyShopStoreApp/MyShopStoreApp/Program.cs
@@ -0,0 +1,66 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+using MyShopStoreApp;
+
+PrintInstructions();
+var file = new FileManager("Products.csv");
+string line;
+do
+{
+    Console.WriteLine($"Wpisz kolejny produkt w postaci danych rozdzielanych średnikami.");
+    line = Console.ReadLine();
+    if (line == "q")
+        break;
+    try
+    {
+        var result = TestInput(line);
+        if (result == ";;;;")
+            file.AddLineToFile(line);
+        else if (result == "-n ")
+            Console.WriteLine(line);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Exeption catched: {ex.Message}");
+    }
+}
+while (line != "q");
+
+List<string> list = file.ReadLineListFromFile();
+foreach (string item in list)
+{
+    Console.WriteLine(item);
+    ////Console.WriteLine(item.Contains("sok"));
+}
+
+
+void PrintInstructions()
+{
+    Console.WriteLine("Dodaj produkt do magazynu lub modyfikuj cenę jednostkową i/lub ilość wybranego produktu.");
+    Console.WriteLine("syntax1: -n [unikatowa nazwa produktu] -c [cena jednostkowa w zł] -i [ilość sztuk] -m [ilość masowa] -p [jednostka kg lub l]");
+    Console.WriteLine("syntax2: [unikatowa nazwa produktu];[cena jednostkowa w zł];[ilość sztuk];[masa lub objętość];[jednostka w kg lub l]");
+    Console.WriteLine("Ceny podawaj w postaci liczb całkowitych lub zmiennoprzecinkowych, np.: 29,99");
+    Console.WriteLine("Masę lub objętość podawaj w postaci liczb całkowitych lub zmiennoprzecinkowych, np.: 3, czy 1,5");
+    Console.WriteLine("Nazwę produktu podawaj bez spacji i bez polskich znaków.");
+    Console.WriteLine("Jeśli produkt jest na wagę, jako ilość podawaj zawsze 1.");
+    Console.WriteLine("Aby wyjść z programu, naciśnij q.");
+}
+string TestInput(string input)
+{
+    string result;
+    var arr1 = input.Split(";");
+    var arr2 = input.Split(" ");
+    if (arr1.Length == 5)
+    {
+        result = ";;;;";
+    }
+    else if (input.Contains("-n ") && arr2.Length == 4)
+    {//-n nazwa -c cena, -n nazwa -i ilosc
+        result = "-n ";
+    }
+    else
+    {
+        result = "";
+    }
+    return result;
+}
